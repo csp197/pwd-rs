e995559e81120eb9c4c527b5f7cad748f2a0a043
CSP <16367956+CSP197@users.noreply.github.com>
2024-01-18 13:05:24 -0800

    MVP v1 p1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::{env, path::PathBuf};
+use std::{env, io};
 
-fn get_current_working_directory() -> std::io::Result<PathBuf> {
-    let path = env::current_dir()?;
+fn get_current_working_directory() {
+    let path = env::current_dir().unwrap();
     println!("{}", path.display());
-    Ok(path)
 }
 
 fn main() {
-    let _ = get_current_working_directory();
+    let mut buf: String = String::new();
+    let _n: usize = io::stdin().read_line(&mut buf).unwrap();
+    match buf.as_str() {
+        "-L\n" => get_current_working_directory(),
+        "-P\n" => println!("-P flag detected!"),
+        _ => println!("No flag detected... => {}", buf),
+    }
+    // let _ = get_current_working_directory();
     // println!("{:?}",path);
 }
