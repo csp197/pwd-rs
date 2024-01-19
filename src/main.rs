273c89fff3658d8f3b40955764cf3ea7d79d29c0
CSP <16367956+CSP197@users.noreply.github.com>
2024-01-18 20:12:17 -0800

    Testing needed...

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
 use std::{env, io};
 
-fn get_current_working_directory() {
-    let path = env::current_dir().unwrap();
+fn get_logical_current_working_directory(var_name: String) {
+    println!("Line 4 => {}", var_name);
+}
+
+fn get_physical_current_working_directory() {
+    let current_dir: Result<std::path::PathBuf, io::Error> = env::current_dir();
+    let path: std::path::PathBuf = match current_dir {
+        Ok(d) => d,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
     println!("{}", path.display());
 }
 
 fn main() {
-    let mut buf: String = String::new();
-    let _n: usize = io::stdin().read_line(&mut buf).unwrap();
-    match buf.as_str() {
-        "-L\n" => get_current_working_directory(),
-        "-P\n" => println!("-P flag detected!"),
-        _ => println!("No flag detected... => {}", buf),
+    const ENV_VAR_NAME: &str = "PWD";
+
+    let args: Vec<String> = env::args().collect();
+
+    let mut flag: &str = "";
+    if args.len() >= 2 {
+        flag = &args[1];
     }
-    // let _ = get_current_working_directory();
-    // println!("{:?}",path);
+    match flag {
+        "-L" => println!(
+            "{:?}",
+            get_logical_current_working_directory(ENV_VAR_NAME.to_string())
+        ),
+        "-P" => println!("{:?}", get_physical_current_working_directory()),
+        _ => println!(
+            "Line 33 => {:?}",
+            get_logical_current_working_directory(ENV_VAR_NAME.to_string())
+        ),
+    }
 }
