0ff45465b6a1087e04005d07e91748ead83a7915
CSP <16367956+CSP197@users.noreply.github.com>
2024-01-20 10:36:04 -0800

    Housekeeping p3

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,21 @@ mod tests;
 
 use std::{env, io};
 
+fn print_err_and_exit(msg: String) {
+    eprintln!("{}", msg);
+    std::process::exit(1);
+}
+
 fn get_logical_current_working_directory(env_var: String) -> String {
-    let var_key: Result<String, env::VarError> = env::var(env_var.clone());
-    let val: String = match var_key {
-        Ok(v) => v,
-        Err(err) => panic!("pwd-rs: ${} is not set: {}", env_var, err),
-    };
-    return val;
+    let var_key: String =
+        env::var(env_var.clone()).expect(format!("pwd-rs: ${env_var} is not set").as_str());
+    return var_key;
 }
 
 fn get_physical_current_working_directory() -> String {
-    let current_dir: Result<std::path::PathBuf, io::Error> = env::current_dir();
-    let path_buf: std::path::PathBuf = match current_dir {
-        Ok(d) => d,
-        Err(err) => panic!(
-            "pwd-rs: physical current working directory not found: {}",
-            err
-        ),
-    };
-    return path_buf.into_os_string().to_str().unwrap().to_string();
+    let current_dir: std::path::PathBuf =
+        env::current_dir().expect("pwd-rs: physical current working directory not found");
+    return current_dir.into_os_string().to_str().unwrap().to_string();
 }
 
 fn main() {
@@ -30,8 +26,7 @@ fn main() {
     if args.len() >= 2 {
         flag = &args[1];
         if !flag.starts_with('-') {
-            println!("pwd-rs: too many arguments: {}", flag);
-            std::process::exit(1)
+            print_err_and_exit(format!("pwd-rs: too many arguments: {flag}"))
         }
     }
     match flag {
@@ -40,7 +35,7 @@ fn main() {
             get_logical_current_working_directory(ENV_VAR_NAME.to_string())
         ),
         "-P" => println!("{}", get_physical_current_working_directory()),
-        _ => println!("pwd-rs: bad option: {}", flag),
+        _ => print_err_and_exit(format!("pwd-rs: bad option: {flag}")),
     }
     std::process::exit(0);
 }
